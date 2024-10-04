949de0ed1042219ec0417572dd2c45b845fcc8af
Ryan Johnson <ryjoh@stanford.edu>
2024-10-04 16:06:41 -0700

    progress

---
+++ test/test_replace.py
@@ -0,0 +1,8 @@
+f = open('test_read.h.in','r')
+scalar = "double"
+with f as file:
+    # Read the file content
+    content = file.read()
+print(content)
+new_content  = eval(f'f"""{content}"""')
+print(new_content)
