658cfeb1d1669d0725d425ca838f62c1b7b08269
Ryan Johnson <ryjoh@stanford.edu>
2024-11-28 16:28:12 -0800

    almost there

--- bin/modules/reactions/pressure_dependent_arrhenius.py
+++ bin/modules/reactions/pressure_dependent_arrhenius.py
@@ -26,16 +26,45 @@ def pressure_dependent_arrhenius_text(reaction_index, As, Bs, Es, pressures, spe
     indentation = '        '
     scalar_cast = '{scalar_cast}'.format(**vars(configuration))
     scalar = '{scalar}'.format(**vars(configuration))
-    for k, [A, B, E, pressure] in enumerate(zip(As, Bs, Es, pressures)):
+    
+    previous_pressure = pressures[0]
+    rates = []
+    rate_at_pressure = []
+    unique_pressures = []
+    for k, pressure in enumerate(pressures):
+        E = Es[k]; A  = As[k]; B = Bs[k]
+        rate_at_pressure.append(f"arrhenius({scalar_cast}({A}), {scalar_cast}({B}), {scalar_cast}({E}), temperature)")
+        if pressure == previous_pressure:
+            continue
+        else:
+            unique_pressures.append(pressure)
+            rates.append(rate_at_pressure)
+            rate_at_pressure = []
+        previous_pressure = pressure
+
+    if reaction_index == 861:
+        for k, rate in enumerate(rates):
+            print(unique_pressures[k])
+            print(rate)
+        
+    rates_for_write = []
+    for rate in rates:
+        if len(rate)>1:
+            rates_for_write.append(' + '.join(rate))
+        else:
+            rates_for_write.append(rate[0])
+
+
+    for k, pressure in enumerate(unique_pressures):
         if k ==0:
-            choose_text +=f"{indentation}/**/ if (log_pressure < {np.log(pressure)}) {{ return arrhenius({scalar_cast}({A}), {scalar_cast}({B}), {scalar_cast}({E}), temperature); }}\n"
+            choose_text +=f"{indentation}/**/if (log_pressure < {np.log(pressure)}) {{ return {rates_for_write[k]}; }}\n"
         else:
             choose_text +=(f"{indentation}else if ({np.log(pressures[k-1])} <= log_pressure && log_pressure < {np.log(pressure)})"
             f"\n{indentation}{{"
-            f"\n{indentation}{scalar} log_k1 = log_arrhenius({np.log(As[k-1])}, {Bs[k-1]}, {Es[k-1]}, log_temperature, inv_universal_gas_constant_temperature);"
-            f"\n{indentation}{scalar} log_k2 = log_arrhenius({np.log(A)}, {B}, {E}, log_temperature, inv_universal_gas_constant_temperature); "
-            f"\n{indentation}return pressure_dependent_arrhenius(log_k1, log_k2, log_pressure,  {np.log(pressures[k-1])}, {np.log(pressure)});\n{indentation}}}\n")
-    choose_text +=f"\n{indentation}else {{ return arrhenius({scalar_cast}({As[-1]}), {scalar_cast}({Bs[-1]}), {scalar_cast}({Es[-1]}), temperature, log_temperature); }}"
+            f"\n{indentation}{scalar} log_k1 = log_gen({rates_for_write[k-1]});"
+            f"\n{indentation}{scalar} log_k2 = log_gen({rates_for_write[k]}); "
+            f"\n{indentation}return pressure_dependent_arrhenius(log_k1, log_k2, log_pressure,  {np.log(unique_pressures[k-1])}, {np.log(pressure)});\n{indentation}}}\n")
+    choose_text +=f"\n{indentation}else {{ return {rates_for_write[-1]}; }}"
 
     return_text ="""
 {device_option}
@@ -44,7 +73,9 @@ call_forward_reaction_{reaction_index}({species_parameter} species, {scalar_para
 {{
         {scalar} inv_universal_gas_constant_temperature = inv(universal_gas_constant() * temperature); 
         {scalar} log_pressure = log_gen(pressure);
+        {scalar} rate = {scalar_cast}(0);
 {choose_text}
+        return rate;
 }}
     """
     return return_text.format(**vars(configuration), reaction_index = reaction_index, choose_text = choose_text)
