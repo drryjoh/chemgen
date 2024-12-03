d0c9f34d5b02da2f1a85213539e413ecc53a9ae8
Ryan Johnson <ryjoh@stanford.edu>
2024-12-03 11:00:28 -0800

    guts hacked in tutorials

--- tutorial/chemgen_reaction_error/bad_reactions.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-import cantera as ct
-reactions = [ 184,  538,  584,  770,  911,  945,  990, 1008]
-
-gas = ct.Solution("FFCM2_model.yaml")
-
-for reaction_index in reactions:
-    print(gas.reactions()[reaction_index].reaction_type)
