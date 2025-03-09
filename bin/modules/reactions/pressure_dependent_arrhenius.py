c5ddec7ae4503606ab7b4009b7129b9c001646dd
Ryan Johnson <ryjoh@stanford.edu>
2025-03-09 14:57:26 -0700

    progress on pressure log

--- bin/modules/reactions/pressure_dependent_arrhenius.py
+++ bin/modules/reactions/pressure_dependent_arrhenius.py
@@ -31,7 +31,9 @@ def pressure_dependent_arrhenius_text(reaction_index, As, Bs, Es, pressures, spe
     
     previous_pressure = pressures[0]
     rates = []
+    drates_dtemperature = []
     rate_at_pressure = []
+    drate_at_pressure_dtemperature = []
     unique_pressures = [pressures[0]]
 
     for k, pressure in enumerate(pressures):
@@ -41,10 +43,13 @@ def pressure_dependent_arrhenius_text(reaction_index, As, Bs, Es, pressures, spe
         # If the pressure changes, finalize the current list of rates
         if pressure != previous_pressure:
             rates.append(rate_at_pressure)  # Save the rates for the previous pressure
+            drates_dtemperature.append(drate_at_pressure_dtemperature)
             rate_at_pressure = []  # Reset for the new pressure
+            drate_at_pressure_dtemperature = []  # Reset for the new pressure
             unique_pressures.append(pressure)  # Add the new pressure to unique list
         
         rate_at_pressure.append(f"arrhenius({scalar_cast}({A}), {scalar_cast}({B}), {scalar_cast}({E}), temperature)")
+        drate_at_pressure_dtemperature.append(f"darrhenius_dtemperature({scalar_cast}({A}), {scalar_cast}({B}), {scalar_cast}({E}), temperature)")
 
         # Update the previous pressure tracker
         previous_pressure = pressure
@@ -52,6 +57,8 @@ def pressure_dependent_arrhenius_text(reaction_index, As, Bs, Es, pressures, spe
     # Append the last batch of rates after the loop
     if rate_at_pressure:
         rates.append(rate_at_pressure)
+    if drate_at_pressure_dtemperature:
+        drates_dtemperature.append(drate_at_pressure_dtemperature)
 
     
     rates_for_write = []
@@ -60,6 +67,14 @@ def pressure_dependent_arrhenius_text(reaction_index, As, Bs, Es, pressures, spe
             rates_for_write.append(' + '.join(rate))
         else:
             rates_for_write.append(rate[0])
+    
+    drates_for_write_dtemperature = []
+    for rate in drates_dtemperature:
+        if len(rate)>1:
+            drates_for_write_dtemperature.append(' + '.join(rate))
+        else:
+            drates_for_write_dtemperature.append(rate[0])
+
 
     for k, pressure in enumerate(unique_pressures):
         if k ==0:
@@ -72,6 +87,33 @@ def pressure_dependent_arrhenius_text(reaction_index, As, Bs, Es, pressures, spe
             f"\n{indentation}return pressure_dependent_arrhenius(log_k1, log_k2, log_pressure,  {np.log(unique_pressures[k-1])}, {np.log(pressure)});\n{indentation}}}\n")
     choose_text +=f"\n{indentation}else {{ return {rates_for_write[-1]}; }}"
 
+# Temperature derivative
+    for k, pressure in enumerate(unique_pressures):
+        print(k)
+        print(drates_for_write_dtemperature[k])
+        if k ==0:
+            dchoose_text_dtemperature +=f"{indentation}/**/if (log_pressure < {np.log(pressure)}) {{ return {drates_for_write_dtemperature[k]}; }}\n"
+        else:
+            dchoose_text_dtemperature +=(f"{indentation}else if ({np.log(unique_pressures[k-1])} <= log_pressure && log_pressure < {np.log(pressure)})"
+            f"\n{indentation}{{"
+            f"\n{indentation}{scalar} log_k1 = log_gen({rates_for_write[k-1]});"
+            f"\n{indentation}{scalar} log_k2 = log_gen({rates_for_write[k]}); "
+            f"\n{indentation}{scalar} dlog_k1_dtemperature = log_chain({rates_for_write[k-1]},{drates_for_write_dtemperature[k-1]});"
+            f"\n{indentation}{scalar} dlog_k2_dtemperature = log_chain({rates_for_write[k]},  {drates_for_write_dtemperature[k]}); "
+            f"\n{indentation}return dpressure_dependent_arrhenius_dtemperature(log_k1, dlog_k1_dtemperature, log_k2, dlog_k2_dtemperature, log_pressure,  {np.log(unique_pressures[k-1])}, {np.log(pressure)});\n{indentation}}}\n")
+    dchoose_text_dtemperature +=f"\n{indentation}else {{ return {drates_for_write_dtemperature[-1]}; }}"
+
+# pressure derivative
+    for k, pressure in enumerate(unique_pressures):
+        if k ==0:
+            dchoose_text_dpressure +=f"{indentation}/**/if (log_pressure < {np.log(pressure)}) {{ return {scalar_cast}(0); }}\n"
+        else:
+            dchoose_text_dpressure +=(f"{indentation}else if ({np.log(unique_pressures[k-1])} <= log_pressure && log_pressure < {np.log(pressure)})"
+            f"\n{indentation}{{"
+            f"\n{indentation}{scalar} log_k1 = log_gen({rates_for_write[k-1]});"
+            f"\n{indentation}{scalar} log_k2 = log_gen({rates_for_write[k]}); "
+            f"\n{indentation}return dpressure_dependent_arrhenius_dpressure(log_k1, log_k2, log_pressure, dlog_pressure_dpressure {np.log(unique_pressures[k-1])}, {np.log(pressure)});\n{indentation}}}\n")
+    dchoose_text_dpressure +=f"\n{indentation}else {{ return {scalar_cast}(0); }}"
     return_text ="""
 {device_option}
 {scalar_function}
@@ -100,6 +142,7 @@ dcall_forward_reaction_{reaction_index}_dtemperature({scalar_parameter} temperat
 dcall_forward_reaction_{reaction_index}_dlog_temperature({scalar_parameter} temperature, {scalar_parameter} pressure) {const_option}
 {{
         {scalar} inv_universal_gas_constant_temperature = inv_gen(universal_gas_constant() * temperature); 
+        {scalar} dinv_universal_gas_constant_temperature_dtemperature = inv_chain(universal_gas_constant() * temperature, universal_gas_constant()); 
         {scalar} log_pressure = log_gen(pressure);
         {scalar} rate = {scalar_cast}(0);
 {dchoose_text_dtemperature}
@@ -112,6 +155,7 @@ dcall_forward_reaction_{reaction_index}_dpressure({scalar_parameter} temperature
 {{
         {scalar} inv_universal_gas_constant_temperature = inv_gen(universal_gas_constant() * temperature); 
         {scalar} log_pressure = log_gen(pressure);
+        {scalar} dlog_pressure_dpressure = dlog_da(pressure);
         {scalar} rate = {scalar_cast}(0);
 {dchoose_text_dpressure}
         return rate;
