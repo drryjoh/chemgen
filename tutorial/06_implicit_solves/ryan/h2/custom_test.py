64ee9b2353b86435df5072df5d40ebdacf8d1a8b
Ryan Johnson <ryjoh@stanford.edu>
2025-06-24 15:52:18 -0400

    update ryan tests

--- tutorial/06_implicit_solves/ryan/h2/custom_test.py
+++ tutorial/06_implicit_solves/ryan/h2/custom_test.py
@@ -5,40 +5,6 @@ from pathlib import Path
 import yaml
 import numpy as np
 
-def get_test_conditions(chemical_mechanism):
-    config_path = Path('test_configuration.yaml')
-    test_file = 'test_configuration.yaml'
-    config_path = Path(test_file)
-
-    if config_path.exists():
-        with config_path.open('r') as file:
-            configuration = yaml.safe_load(file)
-    else:
-        current_dir = Path(__file__).resolve().parent
-        configuration_filename = current_dir.parent.parent/ 'test/{0}'.format(test_file)
-        print("**Test file test_configuration.yaml not found using /test/test_configuration.yaml **")
-        with open(configuration_filename, 'r') as file:
-            configuration = yaml.safe_load(file)
-    
-    # Extract values from the parsed YAML data
-    test_conditions = configuration.get('test_conditions', {})
-
-    # Extract other required values
-    temperature = test_conditions['temperature']
-    pressure = test_conditions['pressure']
-    species_list = test_conditions['species']
-
-    # Print out the parsed values
-    print("Test configuration:")
-    print(f"Temperature: {temperature}")
-    print(f"Pressure: {pressure}")
-    print("Species:")
-
-    for species in species_list:
-        print(f"  - Name: {species['name']}, MoleFraction: {species['MoleFraction']}")
-    species_string  = ' '.join([f"{species['name']}:{species['MoleFraction']} " for species in species_list])
-    return [temperature, pressure, species_string, False]
-
 def create_test(gas, chemical_mechanism, headers, test_file_name, configuration, destination_folder, n_points = 0):
     test_file = destination_folder/test_file_name
     with open(test_file, 'w') as file:
@@ -64,12 +30,6 @@ std::ostream& operator<<(std::ostream& os, const std::array<T, N>& arr) {
         """)
         for header in headers:
             file.write(f"#include \"{header}\"\n")
-
-        [temperature, pressure, species_string, random] = get_test_conditions(chemical_mechanism)
-        gas.TPX = temperature, pressure, species_string
-        concentrations = gas.concentrations
-
-        concentration_test = '{species} species  = {{{array}}};'.format(array = ','.join(["{scalar_cast}({c})".format(c=c, **vars(configuration)) for c in concentrations]),**vars(configuration)) 
 
         content = """
 #define {internal_energy_or_temperature}
@@ -351,6 +311,4 @@ main()
 
 #undef {internal_energy_or_temperature}
             """
-        file.write(content.format(**vars(configuration), 
-        concentration_test = concentration_test, 
-        temperature = temperature))
+        file.write(content.format(**vars(configuration)))
