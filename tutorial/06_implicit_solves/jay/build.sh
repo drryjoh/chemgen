615d75a2549efdc3f7d092415002212f3dfac438
jay <hawaiian647@gmail.com>
2025-07-02 13:43:34 -0400

    ITER

--- tutorial/06_implicit_solves/jay/build.sh
+++ tutorial/06_implicit_solves/jay/build.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
 #JAYS VERSION
-python3 /Users/jsampa/Code/chemgen/bin/chemgen.py FFCM2_model.yaml . --custom-test custom_test.py --compile
+python3 /Users/jsampa/Code/chemgen/bin/chemgen.py FFCM2_model.yaml bin/ --get-sparsity --plot-sparsity --skip-tests
+
+# #PURE COMPILING
+# BREW_PREFIX=$(brew --prefix yaml-cpp)
+# clang++ -std=c++23 -O2 -I"${BREW_PREFIX}/include" -L"${BREW_PREFIX}/lib" -lyaml-cpp -I$CONDA_PREFIX/include/eigen3 chemgen.cpp -o main
--- tutorial/06_implicit_solves/jay/compile.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-BREW_PREFIX=$(brew --prefix yaml-cpp)
-clang++ -std=c++23 -O2 -I"${BREW_PREFIX}/include" -L"${BREW_PREFIX}/lib" -lyaml-cpp -I$CONDA_PREFIX/include/eigen3 chemgen.cpp -o main
