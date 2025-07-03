0402605d08ee25642ebadd23546ce5cbdffc865e
Ryan Johnson <ryjoh@stanford.edu>
2025-07-02 18:14:48 -0700

    updates paper changes time steps in implicit solve

--- paper/data/01_error/post.py
+++ paper/data/01_error/post.py
@@ -3,7 +3,11 @@ import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-colors = ["purple", "blue", "orange", "red", "green"]
+try:
+    plt.style.use('seaborn-colorblind')
+except OSError:
+    plt.style.use('seaborn-v0_8-colorblind')
+colors = plt.rcParams['axes.prop_cycle'].by_key()['color']
 mechs = ["OConnaire", "burke", "gri30", "sandiego", "FFCM2_model"]
 names = ["Ó Connaire", "Burke", "GRI v3.0", "UCSD", "FFCM2"]
 mean_loc = -2.0  # for degree 7
@@ -23,12 +27,22 @@ for i, mech in enumerate(mechs):
         log_errors,
         ax=ax,
         bins=300,
+        stat='density',
         color=colors[i],
         alpha=0.5,
         edgecolor='black',
         label=names[i],
-        kde=True
-    )
+        kde=False  # disable internal kde
+        )
+
+# Add KDE manually
+    sns.kdeplot(
+        log_errors,
+        ax=ax,
+        color='black',
+        linewidth=0.75,
+        label=None
+        )
 
     mean_val = np.mean(log_errors)
     std_val = np.std(log_errors)
@@ -36,7 +50,7 @@ for i, mech in enumerate(mechs):
     stds.append(std_val)
     ax.axvline(mean_val, color=colors[i], linestyle='--', linewidth=1)
 
-ax.set_ylim([0, 350])
+#ax.set_ylim([0, 350])
 txtloc = np.linspace(0.57, 0.9, len(means))
 for r, mean_val in enumerate(means):
     ax.text(mean_loc, ax.get_ylim()[1]*(txtloc[r]+0.05), 
@@ -49,7 +63,7 @@ ax.set_xticks(integer_ticks)
 ax.set_xticklabels([f"$10^{{{int(t)}}}$" for t in integer_ticks])
 #ax.set_title(f"$n_p={deg}$", fontsize=14)
 ax.set_xlim([-9, 1])
-ax.set_ylabel("Distribution", fontsize=12)
+ax.set_ylabel("Probability Density",fontsize=12)
 ax.set_xlabel(r"$\epsilon$", fontsize=10)
 
 handles, labels = ax.get_legend_handles_labels()
