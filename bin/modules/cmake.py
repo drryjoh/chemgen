beb6a21156b2ece73336e370fdf571401345d2b5
Eric Ching <ericjching@gmail.com>
2025-07-23 10:58:58 -0400

    minor cmake changes

--- bin/modules/cmake.py
+++ bin/modules/cmake.py
@@ -12,7 +12,7 @@ set(CMAKE_CXX_STANDARD 17)
 set(CMAKE_CXX_STANDARD_REQUIRED True)
 
 # Compiler options
-set(CMAKE_CXX_FLAGS "-{config['build'].get('chemgen_optimized', 'O2')}")
+set(CMAKE_CXX_FLAGS "${{CMAKE_CXX_FLAGS}} {config['build'].get('chemgen_input', '')}")
 
 # Source files
 set(SOURCE_FILES
