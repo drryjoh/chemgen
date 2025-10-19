9b8f13512c034debef9861ca7a5b71df1bd0de11
Ryan Johnson <ryjoh@stanford.edu>
2025-10-19 10:03:37 -0400

    compares eigenvalue created

--- paper/PROCI/data/setup_pybind.sh
+++ paper/PROCI/data/setup_pybind.sh
@@ -1,5 +1,5 @@
 #!/bin/zsh
 cg
 chemgen.py ffcm2_h2.yaml chemgen_conservative --pybind 
-chemgen.py ffcm2_h2.yaml chemgen_temperature --pybind  
+chemgen.py ffcm2_h2.yaml chemgen_temperature --pybind --temperature-equation
 python3 ./setup.py build_ext --inplace
